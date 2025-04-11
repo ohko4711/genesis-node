caf390f2f2a1bf4aca3f910f74e4ada4d1f1683f
richard <ohko4711@163.com>
2025-04-11 15:26:48 +0800

    feat(scripts): update sendConsolidationRequest.js to dynamically construct transaction data using source and target public keys

--- scripts/sendConsolidationRequest.js
+++ scripts/sendConsolidationRequest.js
@@ -6,11 +6,14 @@ const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
 async function sendConsolidationRequest() {
 
+    const source = "aa4545eae1ffe47cb4e48d1aa640e7994acdc9c976581c4a1bbf4ec34cbf2cad516e58e31e104a6bc26659cb6617674f";
+    const target = "aa4545eae1ffe47cb4e48d1aa640e7994acdc9c976581c4a1bbf4ec34cbf2cad516e58e31e104a6bc26659cb6617674f";
+
     const tx = await wallet.sendTransaction({
         // consolidation contract address
         to: process.env.CONSOLIDATION_CONTRACT_ADDRESS,
         // data format: 0x{source_pubkey}{target_pubkey}
-        data: "0xaa4545eae1ffe47cb4e48d1aa640e7994acdc9c976581c4a1bbf4ec34cbf2cad516e58e31e104a6bc26659cb6617674fb183ffd1e01fb382d78a6757e726ce3d7d4726bb71175718e7badf717e0b78b1c2b4e0e51bb2bb5069a5027074263f68",
+        data: `0x${source}${target}`,
         gasLimit: 2100000,
         // value: dynamic fee, for test, set a fixed value
         value: ethers.utils.parseEther("0.01")
