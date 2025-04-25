aa2ae19421d59037994f9ed52520e280013115a9
richard <ohko4711@163.com>
2025-04-25 20:20:37 +0800

    fix(mev-boost-relay): clasify EXECUTION_URL useage

--- mev-boost-relay/start.sh
+++ mev-boost-relay/start.sh
@@ -11,7 +11,8 @@ echo "Using IP address: $IP_ADDRESS"
 # mev-builder and mev-boost-relay in same machine
 export BEACON_URLS=http://${IP_ADDRESS}:5052
 
-# rbuilder json-rpc-port
-export EXECUTION_URL=http://${IP_ADDRESS}:8645
+# rbuilder json-rpc-port is used for searcher or normal user send boundle tx
+# for mev-boost relay use EXECUTION_URL which enable rpc flashbots_*** for sim
+export EXECUTION_URL=http://${IP_ADDRESS}:8545
 
 docker compose up -d
